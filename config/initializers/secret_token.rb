916ae0785af7a98c429a04cc15e2b4162e91d080
Zachary Crockett <zac@zacharycrockett.com>
2011-11-15 01:35:32 -0600

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplebudget::Application.config.secret_token = 'b69f29b0364ba25956200242721dc0c3f56e12789a1089bc12f3598de150c11bbd65dc032df5b737434066c4e8d33636e07404e24dc98005b8c06a972c8d1db3'
